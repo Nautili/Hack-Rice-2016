00f47285679e90e3e5e206d47ad3c39aee104bcf
ral9 <ral9@rice.edu>
2016-01-16 10:24:00 -0600

    Added LocationInfo

---
+++ src/movement/LocationInfo.java
@@ -0,0 +1,35 @@
+package movement;
+
+import java.awt.Point;
+
+public class LocationInfo {
+
+  private Point vel;
+  private Point pos;
+
+  public LocationInfo() {
+    vel = new Point(0, 0);
+    pos = new Point(0, 0);
+  }
+
+  public LocationInfo(Point vel, Point pos) {
+    this.vel = vel;
+    this.pos = pos;
+  }
+
+  public Point getVelocity() {
+    return vel;
+  }
+
+  public Point getPosition() {
+    return pos;
+  }
+
+  public void setVelocity(Point vel) {
+    this.vel = vel;
+  }
+
+  public void setPosition(Point pos) {
+    this.pos = pos;
+  }
+}
