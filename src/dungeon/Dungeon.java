df73769855bd90da5eb0e6ddd628c84699ba8649
ral9 <ral9@rice.edu>
2016-01-16 12:04:29 -0600

    Changed dungeon

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -3,4 +3,12 @@ package dungeon;
 public class Dungeon {
 
 	private RoomCollection[][] rooms;
+
+  public Dungeon(int numRooms) {
+    rooms = generateDungeon(numRooms);
+  }
+
+  private RoomCollection[][] generateDungeon(int numRooms) {
+    return new RoomCollection[0][0];
+  }
 }
