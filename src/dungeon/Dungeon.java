d85ecbf10e9910a20628733d3977589fdc0bfe6b
ral9 <ral9@rice.edu>
2016-01-16 13:22:49 -0600

    Added basic file search for dungeon

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -1,14 +1,29 @@
 package dungeon;
 
-public class Dungeon {
+import java.io.File;
 
-	private RoomCollection[][] rooms;
+public class Dungeon{
+
+	private RoomCollection[] rooms;
 
   public Dungeon(int numRooms) {
     rooms = generateDungeon(numRooms);
   }
 
-  private RoomCollection[][] generateDungeon(int numRooms) {
-    return new RoomCollection[0][0];
+  private RoomCollection[] generateDungeon(int numRooms) {
+    RoomCollection[] roomList = new RoomCollection[numRooms];
+    //Get list of rooms to link together
+    File roomDir = new File("src/roomFiles");
+    File[] fileList = roomDir.listFiles();
+
+    //randomly select values from roomList
+
+    for(File f : fileList) {
+      //Load files
+      //Translate to usable format
+      //add to room list
+    }
+
+    return roomList;
   }
 }
